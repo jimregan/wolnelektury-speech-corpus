33634dd15ddb769cea176230aa5ee8cb48fab830
Jim O'Regan <jaoregan@tcd.ie>
2017-09-09 19:12:02 +0100

    fix; add output

--- extract-plwiktionary-ipa.pl
+++ extract-plwiktionary-ipa.pl
@@ -11,33 +11,64 @@ binmode(STDERR, ":utf8");
 my $title = '';
 my $polish_seen = 0;
 while(<>) {
-    if(/== *([^(]*)\(\{\{język polski\}\}\) *==/) {
-        $title = $1;
-        $polish_seen = 1;
-    } elsif (/== *([^(]*)\(\{\{język ([^}]*)\}\}\) *==/) {
-        my $tmptitle = $1;
+    if(/<title>([^<]*)<\/title>/) {
+        $title = trim($1);
+        $polish_seen = 0;
+    }
+    if(/== *(.*)\(\{\{język ([^}]*)\}\}\) *==/) {
         my $lang = $2;
-        if($lang !~ /polski/i) {
-            $polish_seen = 0;
-        } else {
+        if($lang eq 'polski') {
             $polish_seen = 1;
-            $title = $tmptitle;
+        } else {
+            $polish_seen = 0;
         }
     }
     if($polish_seen) {
+        my $orth = '';
+        if(/\{\{ortograficzny\|([^}]*)\}\}/) {
+            $orth = $1;
+        }
         if(/\{\{IPA3\|([^}]*)\}\}/) {
             my $pron = $1;
-            if($pron =~ /\|/) {
-                for my $part (split/\|/, $pron) {
-                    print "$title\t$part\n";
-                }
-            } else {
-                print "$title\t$pron\n";
-            }
-        } 
-        if(/\{\{ortograficzny\|([^}]*)\}\}/) {
-            my $orth = $1;
-            print "ORTH\t$title\t$orth\n";
+            doentry($title, $orth, $pron);
+        } else {
+            doentry($title, $orth, '');
         }
     }
-} 
+}
+
+sub trim {
+    my $var = shift;
+    $var =~ s/^ *//;
+    $var =~ s/ *$//;
+    $var;
+}
+
+sub doentry {
+    my $title = $_[0];
+    my $orth = $_[1];
+    my $pron = $_[2];
+
+    $pron =~ s/\&lt;sup\&gt;j\&lt;\/sup\&gt;/ʲ/g;
+    $pron =~ s/\&lt;sup\&gt;w\&lt;\/sup\&gt;/ʷ/g;
+    $pron =~ s/'''//g;
+
+    # skip in current dump - 'masy' is given the pronunciation of 'ludzie'
+    return if($title eq 'masy');
+    if($title eq 'kipiatok') {
+        print "kipiatok\tcipʲjaˈtɔk\t";
+    }
+    
+    my $orth_out = '';
+    if($pron eq '') {
+        print "$title\t\t$orth\n" if($orth ne '');
+    } else {
+        if($pron =~ /\|/) {
+            for my $part (split/\|/, $pron) {
+                print "$title\t$part\t$orth\n";
+            }
+        } else {
+            print "$title\t$pron\t$orth\n";
+        }
+    }
+}
