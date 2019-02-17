12f9c2fe033a27654f6f7995b77989893c198c68
Jim O'Regan <jaoregan@tcd.ie>
2019-02-17 13:54:53 +0100

    change eol

--- header-replace.sh
+++ header-replace.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 pushd text
-perl ../header-replace.pl balzac-komedia-ludzka-bank-nucingena.txt
-perl ../header-replace.pl fortepian-chopina.txt
-perl ../header-replace.pl boy-swietoszek.txt
-perl ../header-replace.pl wspomnienia-niebieskiego-mundurka.txt
-perl ../header-replace.pl powiesci-fantastyczne-wybor-narzeczonej.txt
-perl ../header-replace.pl gloria-victis-gloria-victis.txt
-perl ../header-replace.pl przygody-tomka-sawyera.txt
-perl ../header-replace.pl ballada-z-tamtej-strony-imieniny.txt
-perl ../header-replace.pl przedwiosnie.txt
-perl ../header-replace.pl piesn-o-rolandzie.txt
+for i in balzac-komedia-ludzka-bank-nucingena.txt fortepian-chopina.txt \
+boy-swietoszek.txt wspomnienia-niebieskiego-mundurka.txt \
+powiesci-fantastyczne-wybor-narzeczonej.txt \
+gloria-victis-gloria-victis.txt przygody-tomka-sawyera.txt \
+ballada-z-tamtej-strony-imieniny.txt przedwiosnie.txt \
+piesn-o-rolandzie.txt
+do
+    perl ../header-replace.pl $i > tmp
+    mv tmp $i
+done
 popd
