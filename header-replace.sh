a8bd834be0ad1f44d1ab77ad63b939138fc9411f
Jim O'Regan <jaoregan@tcd.ie>
2019-02-17 13:46:37 +0100

    simple driver script

---
+++ header-replace.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+pushd text
+perl ../header-replace.pl balzac-komedia-ludzka-bank-nucingena.txt
+perl ../header-replace.pl fortepian-chopina.txt
+perl ../header-replace.pl boy-swietoszek.txt
+perl ../header-replace.pl wspomnienia-niebieskiego-mundurka.txt
+perl ../header-replace.pl powiesci-fantastyczne-wybor-narzeczonej.txt
+perl ../header-replace.pl gloria-victis-gloria-victis.txt
+perl ../header-replace.pl przygody-tomka-sawyera.txt
+perl ../header-replace.pl ballada-z-tamtej-strony-imieniny.txt
+perl ../header-replace.pl przedwiosnie.txt
+perl ../header-replace.pl piesn-o-rolandzie.txt
+popd
