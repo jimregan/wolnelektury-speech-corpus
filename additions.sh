7c3e96f19735df942007b6625c64b180529aa297
Jim Regan <jaoregan@tcd.ie>
2019-03-20 01:30:58 +0100

    Add driver script (#14)
    * ignore text/
    
    * simple driver script
    
    * change eol
    
    * fix header replacement
    
    * drop ^M
    
    * small error (at least when processing Robinson Crusoe
    
    * readd change
    
    * move to single script
    
    * replace another
    
    * start of chapter splitting
    
    * grab rest of texts from splitter
    
    * call steps - step missing before split into chapters
    
    * missing step for chapter numbers
    
    * silence wingit
    
    * make directory specifiable from command line
    
    * oops

--- additions.sh
+++
@@ -1,3 +0,0 @@
-echo 'Tobie, leniu' >> text/but-w-butonierce-deszcz.txt
-echo 'Czytał Wiktor Korzeniewski' >> text/zajac-i-jez.txt
-echo 'Czytał Jacek Rozenek' >> text/przedwiosnie.txt
---
+++ normalise-text.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+broad_norm() {
+    echo "Stage 1: Broad normalisations"
+    for i in *.txt
+    do
+        cat $i | perl ../norm-text.pl > $i.tmp
+        mv $i.tmp $i
+    done
+}
+
+header_replace() {
+    echo "Stage 2: Replace headers"
+    for i in balzac-komedia-ludzka-bank-nucingena.txt fortepian-chopina.txt \
+        boy-swietoszek.txt wspomnienia-niebieskiego-mundurka.txt \
+        powiesci-fantastyczne-wybor-narzeczonej.txt \
+        gloria-victis-gloria-victis.txt przygody-tomka-sawyera.txt \
+        ballada-z-tamtej-strony-imieniny.txt przedwiosnie.txt \
+        piesn-o-rolandzie.txt
+    do
+        echo "Replacing header in $i"
+        perl ../header-replace.pl $i > $i.tmp
+        mv $i.tmp $i
+    done
+}
+
+split_chapters() {
+    echo "Stage 5: Split texts to match audio"
+    for i in balzac-komedia-ludzka-corka-ewy.txt \
+        boy-swietoszek.txt \
+        golem.txt \
+        kim.txt \
+        piesn-o-rolandzie.txt \
+        powiesci-fantastyczne-wybor-narzeczonej.txt \
+        przedwiosnie.txt \
+        przygody-tomka-sawyera.txt \
+        robinson-crusoe.txt \
+        sztuka-kochania.txt \
+        wspomnienia-niebieskiego-mundurka.txt \
+        wyspa-skarbow.txt
+    do
+        echo "Splitting $i"
+        perl ../splitter.pl $i > $i.tmp
+        mv $i.tmp $i
+    done
+}
+
+norm_chapters() {
+    echo "Stage 4: Add missing chapter word"
+    for i in wyspa-skarbow.txt wspomnienia-niebieskiego-mundurka.txt
+    do
+        perl ../norm-roman-by-text.pl $i > $i.tmp
+        mv $i.tmp $i
+    done
+}
+
+additions() {
+    echo "Stage 3: Final additions"
+    echo 'Tobie, leniu' >> but-w-butonierce-deszcz.txt
+    echo 'Czytał Wiktor Korzeniewski' >> zajac-i-jez.txt
+    echo 'Czytał Jacek Rozenek' >> przedwiosnie.txt
+}
+
+run_all() {
+    if [ x$1 != x"" ]
+    then
+    echo "changing to $1"
+        pushd $1
+    fi
+
+    broad_norm
+    header_replace
+    additions
+    norm_chapters
+    split_chapters
+
+    if [ x$1 != x"" ]
+    then
+        popd
+    fi
+}
+
+if [ x$1 != x"" ]
+then
+    run_all $1
+else
+    run_all
+fi
