bf4af1ae89ad69336390e35b2dcf6264e4e7726c
Jim O'Regan <jaoregan@tcd.ie>
2020-04-22 11:42:31 +0200

    fix

--- run-sphinx.sh
+++ run-sphinx.sh
@@ -1,9 +1,6 @@
 LMPLZ=/home/git/kenlm/bin/lmplz.exe
 #TMPPATH=/tmp/conv
 
-#cat pairs.tsv|while read i;do
-#	a=$(echo "$i"|awk -F'\t' '{print $1}')
-#	t=$(echo "$i"|awk -F'\t' '{print $2}')
 a=$1
 t=$2
 
@@ -17,8 +14,6 @@ cat $t |tr ' ' '\n'|perl filter-dict.pl ~/cmusphinx-clarinpl/pl.dic|sort|uniq > 
 cat /tmp/conv/$t.g2p | tr '\t' ' ' > /tmp/conv/$t.g2ps
 cat ~/cmusphinx-clarinpl/pl.dic /tmp/conv/$t.g2ps > /tmp/conv/$t.dic
 printf "$base\t$base.wav\n" > /tmp/conv/fileids
-#pocketsphinx_batch.exe -adcin yes -cepdir . -cepext .wav -ctl /tmp/conv/fileids -dict ~/cmusphinx-ga-teanglann-0.2/ga.dic -hmm ~/cmusphinx-ga-teanglann-0.2/ -lm ~/cmusphinx-ga-teanglann-0.2/ga.lm.bin -ctm $a.ctm
-#pocketsphinx_batch.exe -adcin yes -cepdir . -cepext .wav -ctl /tmp/conv/fileids -dict /tmp/conv/$t.dic -hmm ~/cmusphinx-clarinpl/ -lm /tmp/conv/$t.bin -ctm $a.ctm
-pocketsphinx_batch.exe -adcin yes -remove_silence no -remove_dc yes -cepdir . -cepext .wav -ctl /tmp/conv/fileids -dict /tmp/conv/$t.dic -hmm ~/cmusphinx-clarinpl/ -lm /tmp/conv/$t.bin -ctm $a.3.ctm
+pocketsphinx_batch.exe -adcin yes -remove_silence no -cepdir . -cepext .wav -ctl /tmp/conv/fileids -dict /tmp/conv/$t.dic -hmm ~/cmusphinx-clarinpl/ -lm /tmp/conv/$t.bin -ctm $a.3.ctm
 
 rm "$base.wav"
