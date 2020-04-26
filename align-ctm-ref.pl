d6afd34b5e42b9819041b1471221bbb376d84ffa
Jim O'Regan <jaoregan@tcd.ie>
2020-04-26 12:38:45 +0200

    basic functionality

--- align-ctm-ref.pl
+++ align-ctm-ref.pl
@@ -9,7 +9,7 @@ use Algorithm::NeedlemanWunsch;
 
 open(CTM, "<", $ARGV[0]);
 binmode(CTM, ":utf8");
-open(REF, "<", $ARGV[0]);
+open(REF, "<", $ARGV[1]);
 binmode(REF, ":utf8");
 binmode(STDOUT, ":utf8");
 
@@ -25,11 +25,32 @@ my @wordsref = ();
 while(<REF>) {
     chomp;
     my @words = split/ /;
-    push @wordsref, @words;
-    # this is dumb, but I only care about the size matching
-    my @pts = map { local $_ = 0 } @words;
-    $pts[0] = 1; # first word in sentence
-    $pts[$#pts] = 2; # last word in sentence
-    push @points, @pts;
+    my @wordsmap = map { local $_ = { 'text' => $_ } } @words; 
+    $wordsmap[0]->{'sent_start'} = 1;
+    $wordsmap[$#wordsmap]->{'sent_end'} = 1;
+    push @wordsref, @wordsmap;
 }
 
+my $matcher = Algorithm::NeedlemanWunsch->new(sub {@_==0 ? -1 : $_[0]->{'text'} eq $_[1]->{'text'} ? 1 : -2});
+
+my @output = ();
+my $result = $matcher->align(\@speech, \@wordsref,
+  {
+    align => sub {
+      my $a = shift;
+      my $b = shift;
+      my $out = $speech[$a];
+      if(exists $wordsref[$b]->{'start'}) {
+        $out->{'sent_start'} = $wordsref[$b]->{'sent_start'};
+      }
+      if(exists $wordsref[$b]->{'end'}) {
+        $out->{'sent_end'} = $wordsref[$b]->{'sent_end'};
+      }
+      $out->{'rec'} = 1;
+      unshift @output, $out;
+    },
+    shift_a => sub { unshift @output, $speech[shift] },
+    shift_b => sub { shift; unshift @output, $wordsref[shift] }
+  });
+
+print Dumper @output;
