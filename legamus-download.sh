24b5df2c66305bd17a07c9000bfcbe98efd123ba
Jim O'Regan <jaoregan@tcd.ie>
2020-04-26 20:20:00 +0200

    finish

--- legamus-download.sh
+++ legamus-download.sh
@@ -1,11 +1,30 @@
 
 # https://legamus.eu/blog/?p=48
 lynx -dump https://literat.ug.edu.pl/prusnow/0010.htm > text/widziadla_prus.txt
-wget https://listen.legamus.eu/prus_widziadla/mp3-128/widziadla_prus.mp3 -O audio/widziadla_prus.mp3
+wget https://listen.legamus.eu/prus_widziadla/mp3-128/widziadla_prus.mp3 -O audio/legamus_widziadla_prus.mp3
 
 # https://legamus.eu/blog/?p=251
-https://listen.legamus.eu/zip/boyzelenski_brazownicy.zip?source=mp3-128
-https://wolnelektury.pl/media/book/txt/brazownicy.txt
+#https://listen.legamus.eu/zip/boyzelenski_brazownicy.zip?source=mp3-128
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_00_boyzelenski.mp3 -O audio/legamus_brazownicy_00_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_01_boyzelenski.mp3 -O audio/legamus_brazownicy_01_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_02_boyzelenski.mp3 -O audio/legamus_brazownicy_02_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_03_boyzelenski.mp3 -O audio/legamus_brazownicy_03_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_04_boyzelenski.mp3 -O audio/legamus_brazownicy_04_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_05_boyzelenski.mp3 -O audio/legamus_brazownicy_05_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_06_boyzelenski.mp3 -O audio/legamus_brazownicy_06_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_07_boyzelenski.mp3 -O audio/legamus_brazownicy_07_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_08_boyzelenski.mp3 -O audio/legamus_brazownicy_08_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_09_boyzelenski.mp3 -O audio/legamus_brazownicy_09_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_10_boyzelenski.mp3 -O audio/legamus_brazownicy_10_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_11_boyzelenski.mp3 -O audio/legamus_brazownicy_11_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_12_boyzelenski.mp3 -O audio/legamus_brazownicy_12_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_13_boyzelenski.mp3 -O audio/legamus_brazownicy_13_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_14_boyzelenski.mp3 -O audio/legamus_brazownicy_14_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_15_boyzelenski.mp3 -O audio/legamus_brazownicy_15_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_16_boyzelenski.mp3 -O audio/legamus_brazownicy_16_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_17_boyzelenski.mp3 -O audio/legamus_brazownicy_17_boyzelenski.mp3
+wget https://listen.legamus.eu/boyzelenski_brazownicy/mp3-128/brazownicy_18_boyzelenski.mp3 -O audio/legamus_brazownicy_18_boyzelenski.mp3
+wget https://wolnelektury.pl/media/book/txt/brazownicy.txt -O text/brazownicy.txt
 
 # https://legamus.eu/blog/?p=525
-## Skip text, already in collection
+wget https://listen.legamus.eu/collection_mlsw001/mp3-128/mlsw001_07_pl_nowytarzan_pn.mp3 -O audio/legamus_nowy_tarzan.mp3
