b43830f641b7bd034311d6c07b581843fff7bec2
Jim O'Regan <jaoregan@tcd.ie>
2020-04-28 08:14:20 +0200

    tweak text download

--- legamus-download.sh
+++ legamus-download.sh
@@ -1,6 +1,6 @@
 
 # https://legamus.eu/blog/?p=48
-lynx -dump https://literat.ug.edu.pl/prusnow/0010.htm > text/widziadla_prus.txt
+lynx -dump -width=990 https://literat.ug.edu.pl/prusnow/0010.htm|grep -v '__________________' > text/widziadla_prus.txt
 wget https://listen.legamus.eu/prus_widziadla/mp3-128/widziadla_prus.mp3 -O audio/legamus_widziadla_prus.mp3
 
 # https://legamus.eu/blog/?p=251
