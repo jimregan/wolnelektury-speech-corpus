b076eedd8fc37195248b0083c6a9b0f46f6928d8
Jim O'Regan <jaoregan@tcd.ie>
2020-07-16 11:24:33 +0100

    make reading into a sub

--- ctm-grep.pl
+++ ctm-grep.pl
@@ -11,42 +11,49 @@ my $print_lines = 0;
 
 GetOptions('silence=f' => \$silence, 'print-lines' => \$print_lines);
 
-if($#ARGV != 1) {
+if($print_lines == 0 && $#ARGV != 1) {
 	die "Usage: ctm-grep.pl [--silence] PATTERN CTMFILE\n";
 }
 
-my $pat = $ARGV[0];
-my $file = $ARGV[1];
-
-open(FILE, "<", $file);
 binmode(STDOUT, ":utf8");
-binmode(FILE, ":utf8");
+my $pat = ($print_lines == 0) ? $ARGV[0] : '';
+my $filename = ($print_lines == 0) ? $ARGV[1] : $ARGV[0];
 
-# build lines
-# this depends on the mixed word/phoneme output of one of the clarin-pl scripts
-my @lines = ();
-my $line = "";
-while(<FILE>) {
-	s/\r//;
-	my @parts = split/ /;
-	if($parts[0] =~ /^@/ && $parts[4] eq 'sil' && $parts[3] >= $silence) {
-		if($line ne '') {
-			push @lines, $line;
-			$line = '';
-		}
-		next;
-	} elsif($parts[0] =~ /^@/) {
-		next;
-	} else {
-		if($line eq '') {
-			$line = $parts[4];
+sub read_ctm {
+	my $file = shift;
+	my $sil = shift;
+	open(FILE, "<", $file);
+	binmode(FILE, ":utf8");
+
+	# build lines
+	# this depends on the mixed word/phoneme output of one of the clarin-pl scripts
+	my @lines = ();
+	my $line = "";
+	while(<FILE>) {
+		s/\r//;
+		my @parts = split/ /;
+		if($parts[0] =~ /^@/ && $parts[4] eq 'sil' && $parts[3] >= $silence) {
+			if($line ne '') {
+				push @lines, $line;
+				$line = '';
+			}
+			next;
+		} elsif($parts[0] =~ /^@/) {
+			next;
 		} else {
-			$line .= " $parts[4]";
+			if($line eq '') {
+				$line = $parts[4];
+			} else {
+				$line .= " $parts[4]";
+			}
 		}
 	}
+	return @lines;
 }
 
-for my $l (@lines) {
+my @text = read_ctm($filename, $silence);
+
+for my $l (@text) {
 	if($print_lines == 1) {
 		print "$l\n";
 	} else {
