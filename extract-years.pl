df64988919147315f95605d283cf0dc49c7122a1
Jim O'Regan <jaoregan@tcd.ie>
2020-07-19 09:52:24 +0100

    more

--- extract-years.pl
+++ extract-years.pl
@@ -6,7 +6,7 @@ use utf8;
 
 use FindBin;
 use lib "$FindBin::RealBin";
-use NumberNorm qw(expand_year);
+use NumberNorm qw(expand_year inflect_ordinal_roman);
 
 binmode(STDOUT, ":utf8");
 
@@ -17,19 +17,52 @@ binmode(INPUT, ":utf8");
 my @tmp = split/\//, $filename;
 my $outfile = $tmp[$#tmp];
 
-while(<INPUT>) {
-	if(/(([Ww]|[Pp]o) ([0-9]+) r(?:oku|\.))/) {
-		my $m = $1;
-		my $prep = $2;
-		my $year = $3;
-		my $outyear = expand_year($year, 'loc');
-		print "$outfile\t$m\t$prep $outyear roku\n";
-	} elsif(/(([Ww]|[Pp]o) r(?:oku|\.) ([0-9]+))/) {
-                my $m = $1;
-                my $prep = $2;
-                my $year = $3;
-                my $outyear = expand_year($year, 'loc');
-                print "$outfile\t$m\t$prep roku $outyear\n";
-        }
+my $kings_nom = 'Ludwik|Henryk|Napoleon|Karol|Jerzy|Ferdynand';
+my $kings_gen = 'Ludwika|Henryka|Napoleona|Karola|Jerzego|Ferdynanda';
+my $kings_ins = 'Ludwikiem|Henrykiem|Napoleonem|Karolem|Jerzym|Ferdynandem';
 
+while(<INPUT>) {
+    if(/(([Ww]|[Pp]o) ([0-9]+) r(?:oku|\.))/) {
+        my $m = $1;
+        my $prep = $2;
+        my $year = $3;
+        my $outyear = expand_year($year, 'loc');
+        print "$outfile\t$m\t$prep $outyear roku\n";
+    } elsif(/(([Ww]|[Pp]o) r(?:oku|\.) ([0-9]+))/) {
+        my $m = $1;
+        my $prep = $2;
+        my $year = $3;
+        my $outyear = expand_year($year, 'loc');
+        print "$outfile\t$m\t$prep roku $outyear\n";
+    } elsif(/(([Ww]|[Pp]o) ([IXV]+)) w(?:ieku|\.)/) {
+        my $m = $1;
+        my $ord = $2;
+        my $prep = $3;
+        my $outord = inflect_ordinal_roman($ord, 'm', 'loc');
+        print "$outfile\t$m\t$prep $outord wieku\n";
+    } elsif(/(([Ww]|[Pp]o) w(?:ieku|\.) ([IXV]+))/) {
+        my $m = $1;
+        my $prep = $2;
+        my $ord = $3;
+        my $outord = inflect_ordinal_roman($ord, 'm', 'loc');
+        print "$outfile\t$m\t$prep wieku $outord\n";
+    } elsif(/(($kings_nom) ([IXV]+))/) {
+        my $m = $1;
+        my $king = $2;
+        my $ord = $3;
+        my $outord = inflect_ordinal_roman($ord, 'm', 'nom');
+        print "$outfile\t$m\t$king $outord\n";
+    } elsif(/(($kings_gen) ([IXV]+))/) {
+        my $m = $1;
+        my $king = $2;
+        my $ord = $3;
+        my $outord = inflect_ordinal_roman($ord, 'm', 'nom');
+        print "$outfile\t$m\t$king $outord\n";
+    } elsif(/(($kings_ins) ([IXV]+))/) {
+        my $m = $1;
+        my $king = $2;
+        my $ord = $3;
+        my $outord = inflect_ordinal_roman($ord, 'm', 'nom');
+        print "$outfile\t$m\t$king $outord\n";
+    }
 }
