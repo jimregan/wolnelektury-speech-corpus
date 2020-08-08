e0b53aa205c12e1e8a565fae1f7e6d13a7104685
Jim O'Regan <jaoregan@tcd.ie>
2020-08-08 11:50:31 +0100

    more

--- normalise-text.sh
+++ normalise-text.sh
@@ -257,6 +257,8 @@ text_norm() {
     mv sed.tmp balzac-komedia-ludzka-muza-z-zascianka.txt-03.txt
     cat wspomnienia-niebieskiego-mundurka.txt-09.txt | sed -e 's/pan \*prefesor\*…/pan *profesor*…/;s/\*prefesor\* pozwoli/*profesor* pozwoli/;s/\*prefesorowi\*/*profesorowi*/;' > sed.tmp
     mv sed.tmp wspomnienia-niebieskiego-mundurka.txt-09.txt
+    cat balzac-komedia-ludzka-muza-z-zascianka.txt-05.txt|sed -e 's/\*wirszy\*//' > sed.tmp
+    mv sed.tmp balzac-komedia-ludzka-muza-z-zascianka.txt-05.txt
     echo "Stace 7c: broad normalisations"
     for i in *.txt
     do
