52485c4792c54c49cf4d8c170105be775f99326a
Jim O'Regan <jaoregan@tcd.ie>
2020-08-12 13:04:26 +0100

    add

---
+++ filter-ctm.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use utf8;
+
+open(DICT, '<', $ARGV[0]);
+binmode(DICT, ":utf8");
+binmode(STDIN, ":utf8");
+binmode(STDOUT, ":utf8");
+
+my %dict = ();
+
+while(<DICT>) {
+	chomp;
+	next if(/^$/);
+	my @a = split/\t/;
+	$dict{lc($a[0])} = 1;
+}
+close(DICT);
+
+if($#ARGV >= 1) {
+  open(DICT, '<', $ARGV[1]);
+  binmode(DICT, ":utf8");
+  while(<DICT>) {
+	  chomp;
+	  next if(/^$/);
+	  my @a = split/\t/;
+	  $dict{lc($a[0])} = 1;
+  }
+  close(DICT);
+}
+
+while(<STDIN>) {
+	chomp;
+	my @line = split/ /;
+	my $word = $line[4];
+	if(!exists $dict{lc($word)}) {
+		print "$_\n";
+	}
+}
