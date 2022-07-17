22edbf1ed18bf63b081e4818760acf4097a2b2bd
Jim O'Regan <joregan@kth.se>
2022-07-17 15:59:11 +0200

    katechizm-polskiego-dziecka-polska-mowa.txt

--- normalise-text.sh
+++ normalise-text.sh
@@ -104,6 +104,7 @@ additions() {
     echo 'Czytał Jan Peszek' >> cierpienia-mlodego-wertera.txt
     echo 'Czytał Jan Peszek' >> antek.txt
     echo 'Czytała Danuta Stenka' >> gloria-victis-dziwna-historia.txt
+    echo 'Czytał Tata Mariusz czyli Mariusz Rzepka' >> katechizm-polskiego-dziecka-polska-mowa.txt
     #echo 'Czytał Adam Fidusiewicz' >> pasewicz-dolina-wilda-w-starym-stylu.txt
     #echo 'Wolne Lektury p l' >> pasewicz-dolina-wilda-w-starym-stylu.txt
     #echo 'Zostań naszym Przyjacielem' >> pasewicz-dolina-wilda-w-starym-stylu.txt
@@ -262,6 +263,7 @@ remove_unread_lines() {
     remove_line przedwiosnie.txt-02.txt '^Prends ton chapeau fleuri,$'
     remove_line przedwiosnie.txt-02.txt '^Ta robe blanche…$'
     remove_line diabel-na-wiezy.txt '^tłumaczenie Bolesław Leśmian$'
+    remove_line katechizm-polskiego-dziecka-polska-mowa.txt '^Katechizm polskiego dziecka$'
 }
 
 pre_norm() {
